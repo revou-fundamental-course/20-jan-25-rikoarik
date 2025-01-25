8c1fa4e54b74e2638120e8e3f7058b63d61883ff
rikoarik <rikoarik04@gmail.com>
2025-01-25 19:26:08 +0700

    feat: navbar with toggle button darkmode

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//Toggle darkmode
+const darkModeToggle = document.getElementById('darkModeToggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+  darkModeToggle.textContent = body.classList.contains('dark-mode') ? '☀️' : '🌙';
+});
